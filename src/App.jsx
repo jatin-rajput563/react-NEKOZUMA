c3405a4fe4065ac609bcba4956d364074bc6d7b0
jatin-rajput563 <jkt3298@gmail.com>
2025-06-02 11:55:55 +0530

    starting-story-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Story from "./components/Story";
 
 function App() {
   return (
     <>
       <Header />
+      <Story />
     </>
   );
 }
---
+++ src/components/Story.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Story = () => {
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
+
+export default Story;
