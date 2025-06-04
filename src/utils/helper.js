f398b89691bc1a6262b0b566831b0f383c1037ba
jatin-rajput563 <jkt3298@gmail.com>
2025-06-04 10:10:54 +0530

    all-section-complete

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,12 +1,12 @@
 import Founder from "../assets/images/png/founder.png";
 import CoFounder from "../assets/images/png/co-founder.png";
 import artist from "../assets/images/png/artist.png";
-import Development from "../assets/images/png/Development.png";
-import Position1 from "../assets/images/png/Position-1.png";
-import Position2 from "../assets/images/png/Position-2.png";
-import Position3 from "../assets/images/png/Position-3.png";
-import Position4 from "../assets/images/png/Position-4.png";
-import { Twitter } from './icons'
+import Development from "../assets/images/png/development.png";
+import Position1 from "../assets/images/png/position-1.png";
+import Position2 from "../assets/images/png/position-2.png";
+import Position3 from "../assets/images/png/position-3.png";
+import Position4 from "../assets/images/png/position-4.png";
+import { Twitter } from '../utils/Icons'
 export const IMAGE_DATA = [
     {
         image: Founder,
